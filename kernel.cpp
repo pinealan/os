a94bd6f35bc5b51a249546fe362cd98e0627a925
pinealan <achan961117@gmail.com>
2017-06-12 23:01:38 +0100

    Third lesson: GDT

--- kernel.cpp
+++ kernel.cpp
@@ -1,9 +1,10 @@
 #include "types.h"
+#include "gdt.hpp"
 
 void printf(char* str) {
 	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-	for(int i = 0; str[i] != '\0'; ++i)
+	for (int i = 0; str[i] != '\0'; ++i)
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
